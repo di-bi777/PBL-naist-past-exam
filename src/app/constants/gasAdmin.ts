6bcd8944718633c3a765e919ab230d57492e1d03
furusato33 <furusato.shota.fu3@naist.ac.jp>
2026-02-26 14:23:05 +0900

    .envの中身を追跡対象ファイルに移動

---
+++ src/app/constants/gasAdmin.ts
@@ -0,0 +1,11 @@
+export const GAS_DRIVE_ENDPOINT =
+  'https://script.google.com/macros/s/AKfycbxkUP8mHxPPhjErsPfGoqujWnjswyqxRHFDVvaDNYIs2GbxIULpsi1MaWE9njy22lVj/exec';
+
+export const GAS_APPROVE_ENDPOINT =
+  'https://script.google.com/macros/s/AKfycbwl12VQ5fZPlEf3scICZWdaakqunjnXntpdVmykEmiBsfhHq8xwM7qE0AbugRBbIcNJ/exec';
+
+export const GAS_REJECT_ENDPOINT =
+  'https://script.google.com/macros/s/AKfycbxqH1wCETJOSrBlw72bMeR1iDZBHU8TWyKEhFmKsI_YzaGElFGm3HBzscGfkPFG64rG/exec';
+
+export const GAS_DB_ENDPOINT =
+  'https://script.google.com/macros/s/AKfycbz82oLtCK3eqpzWBEhbIYyL0ur8nm9dC-JJZukY9eBL0opwhg_DQwZjhSLpJi9Q4J_n/exec';
