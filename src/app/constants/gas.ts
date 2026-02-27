4ca15c9f7f2ef4bb01d349e23a487ecf1c67c025
furusato33 <furusato.shota.fu3@naist.ac.jp>
2026-02-27 11:57:00 +0900

    .envに依存しないよう変更

--- src/app/constants/gas.ts
+++ src/app/constants/gas.ts
@@ -1,13 +1,8 @@
-import { GAS_PROXY_PATH, GAS_PROXY_TARGET } from '../../../config/gas';
-
-const defaultEndpoint = import.meta.env.DEV ? GAS_PROXY_PATH : GAS_PROXY_TARGET;
-
 export const GAS_ENDPOINT =
-  (import.meta.env.VITE_GAS_ENDPOINT as string | undefined) ?? defaultEndpoint;
+  'https://script.google.com/macros/s/AKfycbwu2dWLg6spnBXzuJ4RIZPQBgkmRnzreL9ck2lscL_8A1NkJ1KmaebWWyz1b6fA1OhG/exec';
 
 export const GAS_DB_ENDPOINT =
-  (import.meta.env.VITE_GAS_DB_ENDPOINT as string | undefined) ?? GAS_ENDPOINT;
+  'https://script.google.com/macros/s/AKfycbz82oLtCK3eqpzWBEhbIYyL0ur8nm9dC-JJZukY9eBL0opwhg_DQwZjhSLpJi9Q4J_n/exec';
 
 export const GAS_DISPLAY_ENDPOINT =
-  (import.meta.env.VITE_GAS_DISPLAY_ENDPOINT as string | undefined) ??
   'https://script.google.com/macros/s/AKfycbzp7DbzjuS4yY2z-GNzbtAf5sdcNjvyGoqKROk4spMDKj4VM6Ac_K8q-jiJWKIQlaVNbw/exec';
